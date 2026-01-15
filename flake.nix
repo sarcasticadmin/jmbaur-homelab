e80b0243969c3987285f97ae79806fdc085d93c2
Robert James Hernandez <rob@sarcasticadmin.com>
2026-01-15 03:18:17 +0000

    move to upstream nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configurations for the homelab";
 
   inputs = {
-    nixpkgs.url = "github:jmbaur/nixpkgs/jmbaur-nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     git-hooks.url = "github:cachix/git-hooks.nix";
     home-manager.url = "github:nix-community/home-manager";
